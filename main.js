88c34f8a2db6e91c05091627eaf4d2557517c44a
Kallurisravani12 <KalluriSravani12>
2023-04-12 10:08:52 +0200

    My second commit

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 /*Introduction*/
 //Hello World!
-console.log('Hello,world!')
+console.log('Hello,world!');
+
+//Variables
+let greeting= 'Hej från en variabel';
+console.log(greeting);
+greeting =3;
+console.log(greeting);
+greeting ="Hello";
+const PI=3.1415;
+
+//Printing
+//Concatenation
+console.log(greeting +',PI is: '+PI);
+
+//Interpolation and Templating
+console.log(`${greeting},PI is: ${PI}`);
